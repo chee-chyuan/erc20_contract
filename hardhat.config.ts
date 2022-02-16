9f01c0d0ca54a0fe41d15eb8177d223471191b53
chee-chyuan <angchee@live.com.my>
2022-02-16 09:48:48 +0800

    remove comment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,12 +16,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
  const config: HardhatUserConfig = {
   solidity: "0.8.11",
   networks: {
@@ -37,8 +31,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   typechain: {
     outDir: 'src/types',
     target: 'ethers-v5',
-    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    alwaysGenerateOverloads: false, 
+    externalArtifacts: ['externalArtifacts/*.json'],
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
